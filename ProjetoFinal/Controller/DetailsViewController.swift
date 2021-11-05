118ee5c0a02ab58e906f598553e4b6bebde83f88
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-04 21:43:18 -0300

    Alterações na estrutura do projeto

--- ProjetoFinal/Controller/DetailsViewController.swift
+++ ProjetoFinal/Controller/DetailsViewController.swift
@@ -6,6 +6,7 @@
 
 import UIKit
 import Kingfisher
+import CoreData
 
 class DetailsViewController: UIViewController {
     
@@ -30,6 +31,34 @@ class DetailsViewController: UIViewController {
     
     // MARK: Actions
     @IBAction func buttonFavoritar(_ sender: UIButton) {
+        
+//        let context = DataBaseController.persistentContainer.viewContext
+//        let fav = Favoritos(context: context)
+//        do {
+//            guard let name = fav.pokename else { return }
+//
+//            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favoritos")
+//            fetchRequest.predicate =  NSPredicate(format: "pokename =%@", name)
+//            let fetchedPokemons = try context.fetch(fetchRequest)
+//            print(fetchedPokemons.count)
+//
+//            if fetchedPokemons.count == 0 {
+//
+//                fav.pokename = selectedPokemon?.name?.capitalized
+//                fav.pokeimage = selectedPokemon?.sprites?.front_default
+//                fav.pokealtura = Float(selectedPokemon?.height ?? 0)
+//                fav.pokepeso = Float(selectedPokemon?.weight ?? 0)
+//                DataBaseController.saveContext()
+//                let viewController = FavoriteViewController()
+//                navigationController?.pushViewController(viewController, animated: true)
+//
+//            } else {
+//                self.displayAlert(message: "O elefante \(String(describing: selectedPokemon?.name?.capitalized)) já está favoritado")
+//            }
+//        } catch let error {
+//            print(error)
+//        }
+        
         let context = DataBaseController.persistentContainer.viewContext
         let fav = Favoritos(context: context)
         fav.pokename = selectedPokemon?.name?.capitalized
@@ -42,6 +71,15 @@ class DetailsViewController: UIViewController {
     }
     
     // MARK: Methods
+//    private func displayAlert(message: String) {
+//        let alert = UIAlertController(title: "Atenção", message: message, preferredStyle: .alert)
+//        let buttonOk = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+//        alert.addAction(buttonOk)
+//        DispatchQueue.main.async {
+//            self.present(alert, animated: true, completion: nil)
+//        }
+//    }
+    
     private func setupUI() {
         guard let weight = selectedPokemon?.weight else { return }
         guard let height = selectedPokemon?.height else { return }
